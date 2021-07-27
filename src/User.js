7edfea675b7bfd9ecd9692f00c012642d060860e
topshenyi-web <erxu@ctemc.org>
2021-07-27 19:48:41 -0400

    a

---
+++ src/User.js
@@ -0,0 +1,28 @@
+import react, { useEffect, useState } from 'react';
+import Odometer from 'react-odometerjs';
+
+const User = ({id}) =>{
+    let URL = "https://cses.fi/user/"+id;
+    URL = "https://api.codetabs.com/v1/proxy/?quest="+URL;
+    const [numSolved,setNumSolved] = useState(0);
+    const [timesRendered, setTimesRendered] = useState(0);
+    useEffect(()=>{
+        let fetchData =async () =>{
+            const response = await fetch(URL);
+            var el = document.createElement( 'html' );
+            el.innerHTML = await response.text();
+            setNumSolved(parseInt(el.querySelector(`[href='/problemset/user/${id}/']`).innerHTML));
+            // const data = await response.json();
+        }
+        fetchData();
+        setTimeout(()=>{
+            setTimesRendered(timesRendered+1);
+        },1000);
+    },[timesRendered]);
+
+    return <div className="user-container">
+        <Odometer value={numSolved}/>
+    </div>
+}
+
+export default User;
